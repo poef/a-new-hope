9f4e61a34b32375fd5c1c9f3460d6e24d408d1bc
Auke van Slooten <auke@muze.nl>
2020-11-15 09:49:52 +0100

    improved frame size initialization

--- experiments/acme-quarterly-report/seamless.js
+++ experiments/acme-quarterly-report/seamless.js
@@ -21,18 +21,50 @@ body {
     margin: 0;
     width: max-content;
     height: max-content;
+    max-width: 100vw;
+    max-height: 100vh;
 }
 `);
 
 export const seamless = {
     report: function() {
         let size = document.body.getBoundingClientRect();
-        return {width:size.width,height:size.height};
+        return {width:Math.ceil(size.width),height:Math.ceil(size.height)};
+    },
+    resize: function() {
+        let size = seamless.report();
+        Array.from(document.querySelectorAll('iframe[data-uae-seamless]'))
+        .forEach(frame => {
+            frame.addEventListener(
+                'load',
+                e => bus.publish(
+                    'seamless-request-size',
+                    {
+                        'max-size': {
+                            width: size.width-buffer
+                        }
+                    }, 
+                    frame
+                )
+            )
+        });
     }
 }
 
+let size = seamless.report();
+let buffer=10;
+
 bus.subscribe('seamless-request-size', function(event) {
     // a parent document asks for our size
+    let maxSize = event.data.message['max-size'];
+    let maxWidth = maxSize.width ? maxSize.width+'px' : 'unset';
+    let maxHeight = maxSize.height ? maxSize.height+'px' : 'unset';
+    addStyle(`
+body {
+    max-width: ${maxWidth};
+    max-height: ${maxHeight};
+}
+`);
     bus.publish(
         'seamless-report-size', 
         {
@@ -48,12 +80,10 @@ bus.subscribe('seamless-report-size', function(event) {
     let message = event.data.message;
     let frame = document.querySelector('iframe[name="'+message.frame+'"]');
     if (frame) {
-        frame.style.width  = Math.ceil(message.size.width)+'px';
-        frame.style.height = Math.ceil(message.size.height)+'px';
+        frame.style.width  = Math.ceil(message.size.width+buffer)+'px';
+        frame.style.height = Math.ceil(message.size.height+buffer)+'px';
     }
 });
 
-Array.from(document.querySelectorAll('iframe[data-uae-seamless]'))
-.forEach(frame => {
-    frame.addEventListener('load', e => bus.publish('seamless-request-size', {}, frame))
-});
+seamless.resize();
+window.onresize = seamless.resize;
