002a44626f91d6f12e2b60c4295042ee3bbf9e8c
Auke van Slooten <auke@muze.nl>
2020-11-15 14:41:40 +0100

    added debugging list of bus messages added targeting specific source frame/window in subscribe

--- experiments/acme-quarterly-report/bus.js
+++ experiments/acme-quarterly-report/bus.js
@@ -1,41 +1,60 @@
 window.addEventListener('message', function(event) {
     var name = event.data.name;
+    var target = event.data.target ? event.data.target : 'host';
+    var source = event.data.source;
+    if (bus.debug) {
+        console.log('bus:', '['+(source?source:'host')+' -> '+target+']', name, event.data.message);
+    }
     if (name && subscriptions[name]) {
-        subscriptions[name].forEach(c => c(event));
+        subscriptions[name].forEach(data => {
+            if (data.target=='*' || data.target.name==source) {
+                data.callback(event);
+            }
+        });
     }
 });
 
 let subscriptions = {};
 let host = null;
 
+function getTarget(target) {
+    if (!target) {
+        target = host;
+    }
+    let n;
+    if (typeof target == 'string') {
+        n = target;
+        if (target==='host') {
+            target = host;
+        } else if (target!='*') {
+            target = document.querySelector('iframe[name="'+target.replace('"','&quot;')+'"]');
+        }
+    }
+    if (!target) {
+        throw new Error('Could not find an iframe with name '+n);
+    }
+    return target;
+}
+
 export const bus = {
-    subscribe: function(messageName, callback) {
+    debug: true,
+    subscribe: function(messageName, callback, target='*') {
         if (!subscriptions[messageName]) {
             subscriptions[messageName] = [];
         }
-        subscriptions[messageName].push(callback);
+        target = getTarget(target);
+        subscriptions[messageName].push({
+            callback: callback,
+            target: target
+        });
     },
-    unsubscribe: function(messageName, callback) {
+    unsubscribe: function(messageName, callback, target='*') {
         if (!subscriptions[messageName]) { return }
-        subscriptions[messagename] = subscriptions[messageName].filter(c => c==callback);
+        subscriptions[messagename] = subscriptions[messageName].filter(data => data.callback==callback && data.target==target);
     },
     publish: function(messageName, message, target=null, transfer=[]) {
-        if (!target) {
-            target = host;
-        }
-        let w,n;
-        if (typeof target == 'string') {
-            n = target;
-            if (target==='host') {
-                target = host;
-            } else {
-                target = document.querySelector('iframe[name="'+target.replace('"','&quot;')+'"]');
-            }
-        }
-        if (!target) {
-            throw new Error('Could not find an iframe with name '+n);
-        }
-
+        target = getTarget(target);
+        let w;
         if (target.contentWindow) {
             w = target.contentWindow;
         } else {
@@ -45,6 +64,7 @@ export const bus = {
             name: messageName,
             origin: ''+window.location.href,
             target: target.name,
+            source: window.name,
             message: message
         }, "*", transfer);
     }
