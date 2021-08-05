901f9a26055c2fe293c412c50cbbd1c3648dfb61
Auke van Slooten <auke@muze.nl>
2021-08-05 12:59:42 +0200

    added api descriptions removed scripts() method, merged with script()

--- js/hope.inspector.js
+++ js/hope.inspector.js
@@ -1,19 +1,30 @@
 // inspector prototype
 
 let hope = {
-	doclets: function() {
-		let result = {};
-		Object.keys(document.hope.doclets).forEach(name => { 
-			let status = document.hope.doclets[name].status;
-			result[name] = status;
-		});
-		return result;
+	doclets: function(name=null) {
+		if (name) {
+			return document.hope.doclets[name];
+		}
+		return Object.values(document.hope.doclets);
 	},
 	debugger: function() {
 		document.hope.bus.debug = !document.hope.bus.debug; //toggle
 		return document.hope.bus.debug;
 	},
-	script: function(id) {
+	script: function(id=null, value=null) {
+		if (!id) {
+			let scripts = document.querySelectorAll('script[id]');
+			return Array.from(scripts).map(s => s.id);
+		}
+		if (value) {
+			let script = document.querySelector('script[id='+id+']');
+			script.remove();
+			script = document.createElement('script');
+			script.id = id;
+			document.body.appendChild(script);		
+			script.innerHTML = value;
+			return true;
+		}
 		// if a script with this id doesn't exist yet, then
 		// create a new script tag with the given name as id
 		// open a new tab with an editor with the contents of this script tag
@@ -37,17 +48,25 @@ let hope = {
 		editorWindow.focus();
 		return editorWindow;
 	},
-	updateScript: function(id, value) {
-		let script = document.querySelector('script[id='+id+']');
-		script.remove();
-		script = document.createElement('script');
-		script.id = id;
-		document.body.appendChild(script);		
-		script.innerHTML = value;
-	},
-	scripts: function() {
-		let scripts = document.querySelectorAll('script[id]');
-		return Array.from(scripts).map(s => s.id);
+	tree: function() {
+		//TODO: this call is now automatically forwarded to child doclets
+		//but to support reflection and other stuff we should be able
+		//to forward any call to a sub sub doclet and get the result back
+		let tree = {};
+		let promises = [];
+		hope.doclets().forEach(d => {
+			promises.push(
+				d.api('/x/hope/inspector/')
+				 .tree()
+				 .then(m => { 
+				 	tree[d.name] = m;
+				 	return m;
+				 })
+			);
+		});
+		return Promise.all(promises).then(() => {
+			return tree;
+		});
 	}
 }
 
@@ -71,11 +90,13 @@ window.setTimeout(() => {
 	document.hope.bus.hosted().then(() => {
 		let inspectorApi = document.hope.api.register('/x/hope/inspector/');
 		inspectorApi.doclets = {
+			description: "Returns an array of doclets embedded inside this doclet.",
 			callback: function() {
 				return hope.doclets();
 			}
 		}
 		inspectorApi.debugger = {
+			description: "Toggles the bus console debug output on or off inside this doclet.",
 			callback: function() {
 				//FIXME: this breaks, as debugger() returns a boolean, not an object
 				// and bus.reply tries to add a property 'replyTo' to the return message
@@ -84,13 +105,27 @@ window.setTimeout(() => {
 			}
 		}
 		inspectorApi.script = {
+			description: `With no params, this will return a list of scripts inside this doclet.
+With only the params.id set to the id of a script, it will open an editor for that script.
+With the params.value set, it will update the script to the given value and reload it.`,
+			params: {
+				id: {
+					type: "string",
+					description: "The id of a script to edit or update"
+				},
+				value: {
+					type: "string",
+					description: "The contents to write to the script"
+				}
+			},
 			callback: function(params) {
-				return hope.script(params.id);
+				return hope.script(params.id, params.value);
 			}
 		}
-		inspectorApi.scripts = {
+		inspectorApi.tree = {
+			description: "Returns an object with the tree structure of (active) doclets.",
 			callback: function() {
-				return hope.scripts();
+				return hope.tree();
 			}
 		}
 	});
