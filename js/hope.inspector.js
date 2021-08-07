f2af6286d6d3168273a5dba44699aa13ebb1ab8a
Auke van Slooten <auke@muze.nl>
2021-08-07 12:01:36 +0200

    window.hope is now updated automatically whenever the doclet tree changes it no longer uses a Proxy, and you can call api() and inspector methods on sub doclets (and sub sub doclets, etc)

--- js/hope.inspector.js
+++ js/hope.inspector.js
@@ -1,6 +1,6 @@
 // inspector prototype
-
 let hope = {
+	help: 'Available methods: doclets(name|optional), debugger(), script("id|optional","value|optional"), tree()',
 	doclets: function(name=null) {
 		if (name) {
 			return document.hope.doclets[name];
@@ -9,7 +9,7 @@ let hope = {
 	},
 	debugger: function() {
 		document.hope.bus.debug = !document.hope.bus.debug; //toggle
-		return document.hope.bus.debug;
+		return { debugger: document.hope.bus.debug }; //@FIXME: must return an object, as bus.reply wants to add the property 'replyTo', switch to header/body seperation in message
 	},
 	script: function(id=null, value=null) {
 		if (!id) {
@@ -38,7 +38,7 @@ let hope = {
 		let editor = `
 <strong>editor ${id}</strong><br><textarea id="script_${id}"></textarea><button onClick="save()">Save</button>`;
 		let save = `function save() {
-	window.opener.hope.updateScript('${id}', document.getElementById('script_${id}').value);
+	window.opener.hope.script('${id}', document.getElementById('script_${id}').value);
 }`;
 		let editorWindow = window.open('',id);
 		editorWindow.document.body.innerHTML = editor;
@@ -46,7 +46,7 @@ let hope = {
 		saveScript.innerHTML = save;
 		editorWindow.document.body.appendChild(saveScript);
 		editorWindow.focus();
-		return editorWindow;
+		return {};
 	},
 	tree: function() {
 		//TODO: this call is now automatically forwarded to child doclets
@@ -54,19 +54,31 @@ let hope = {
 		//to forward any call to a sub sub doclet and get the result back
 		let tree = {};
 		let promises = [];
-		hope.doclets().forEach(d => {
+		hope.doclets().forEach(doclet => {
 			promises.push(
-				d.api('/x/hope/inspector/')
-				 .tree()
-				 .then(m => { 
-				 	tree[d.name] = m;
-				 	return m;
-				 })
+				doclet.api('/x/hope/inspector/')
+				.tree()
+				.then(subtree => { 
+					tree[doclet.name] = Object.assign(doclet, subtree, {
+						debugger: () => doclet.api('/x/hope/inspector/').debugger(),
+						script: (...params) => doclet.api('/x/hope/inspector/').script.apply(null, params)
+					});
+					 //@TODO: check security risks by passing Doclet object to parent documents
+					return subtree;
+				})
 			);
 		});
 		return Promise.all(promises).then(() => {
+			Object.keys(tree).forEach(name => {
+				hope[name] = tree[name];
+			})
 			return tree;
 		});
+	},
+	updateTree: function(tree) {
+		if (document.hope.host) {
+			document.hope.host.api('/x/hope/inspector/').updateTree({tree: tree});
+		}
 	}
 }
 
@@ -81,20 +93,12 @@ let apiHandler = {
 	}
 };
 
-let inspector = new Proxy(hope, apiHandler);
-
-window.hope = inspector;
-export default inspector;
+window.hope = hope;
+export default hope;
 
 window.setTimeout(() => {
+	let inspectorApi = document.hope.api.register('/x/hope/inspector/');
 	document.hope.bus.hosted().then(() => {
-		let inspectorApi = document.hope.api.register('/x/hope/inspector/');
-		inspectorApi.doclets = {
-			description: "Returns an array of doclets embedded inside this doclet.",
-			callback: function() {
-				return hope.doclets();
-			}
-		}
 		inspectorApi.debugger = {
 			description: "Toggles the bus console debug output on or off inside this doclet.",
 			callback: function() {
@@ -109,7 +113,7 @@ window.setTimeout(() => {
 With only the params.id set to the id of a script, it will open an editor for that script.
 With the params.value set, it will update the script to the given value and reload it.`,
 			params: {
-				id: {
+				name: {
 					type: "string",
 					description: "The id of a script to edit or update"
 				},
@@ -119,7 +123,7 @@ With the params.value set, it will update the script to the given value and relo
 				}
 			},
 			callback: function(params) {
-				return hope.script(params.id, params.value);
+				return hope.script(params.name, params.value);
 			}
 		}
 		inspectorApi.tree = {
@@ -129,4 +133,31 @@ With the params.value set, it will update the script to the given value and relo
 			}
 		}
 	});
-});
+	inspectorApi.updateTree = {
+		callback: function(params, sourceFrame) {
+			hope[sourceFrame.name] = params.tree;
+		}
+	}
+
+	document.hope.bus.subscribe('/x/hope/bus/init/ready/', event => {
+		let source = event.data.source;
+		if (source) {
+			let doclet = document.hope.doclets[source];
+			if (doclet) {
+				doclet.api('/x/hope/inspector/').tree()
+				.then(subtree => {
+					delete subtree.replyTo; //@FIXME: add header/envelope to messages so we don't have to do this
+					hope[doclet.name] = Object.assign(doclet, subtree, {
+						debugger: () => doclet.api('/x/hope/inspector/').debugger(),
+						script: (id=null, value=null) => doclet.api('/x/hope/inspector/').script({ name: id, value: value})
+					});
+					return subtree;
+				}).then(subtree => {
+					document.hope.bus.hosted().then(() => {
+						document.hope.host.api('/x/hope/inspector/').updateTree({tree:subtree});
+					});
+				});
+			}
+		}
+	});
+},1);
